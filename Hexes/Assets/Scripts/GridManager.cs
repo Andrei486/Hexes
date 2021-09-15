618b4ce364285fbb89d6d298e7ecd9916ea448d3
Andrei486 <apopescu486@gmail.com>
2021-09-15 14:48:14 -0400

    Added hex grid system and cursor movement

---
+++ Hexes/Assets/Scripts/GridManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GridManager : MonoBehaviour
+{
+    //Array of Vector2Ints indicating which spaces on a grid are adjacent to a given space.
+    //Could be simplified by auto-generating the second half, which is just the first half * -1.
+    public static Vector2Int[] adjacencies = {
+        new Vector2Int(1, 0),
+        new Vector2Int(0, 1),
+        new Vector2Int(-1, 1),
+        new Vector2Int(-1, 0),
+        new Vector2Int(0, -1),
+        new Vector2Int(1, -1),
+    };
+    [SerializeField] private Vector3 columnOffsetVector;
+    [SerializeField] private Vector3 rowOffsetVector;
+    [SerializeField] private GameObject hexPrefab;
+    private Dictionary<Vector2Int, GridSpace> gridSpaces;
+    public GameObject tokenPrefab;
+    public TokenColor[] tokenColors;
+    private static GridManager gridManager;
+    void Start()
+    {
+        InitializeGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitializeGrid() {
+        gridSpaces = new Dictionary<Vector2Int, GridSpace>();
+        CreateSpace(Vector2Int.zero, GridSpace.SpaceOccupation.Red);
+        CreateSpace(Vector2Int.right, GridSpace.SpaceOccupation.Blue);
+        ExpandGrid();
+    }
+
+    void ExpandGrid() {
+        Vector2Int adjacentPosition;
+        HashSet<Vector2Int> spacesToAdd = new HashSet<Vector2Int>();
+        foreach (KeyValuePair<Vector2Int, GridSpace> entry in gridSpaces) {
+            if (entry.Value.Occupation != GridSpace.SpaceOccupation.Free) {
+                foreach (Vector2Int adjacency in GridManager.adjacencies) {
+                    adjacentPosition = entry.Key + adjacency;
+                    if (!gridSpaces.ContainsKey(adjacentPosition)) {
+                        spacesToAdd.Add(adjacentPosition);
+                    }
+                }
+            }
+        }
+        foreach (Vector2Int position in spacesToAdd) {
+            CreateSpace(position, GridSpace.SpaceOccupation.Free);
+        }
+    }
+
+    private void CreateSpace(Vector2Int position, GridSpace.SpaceOccupation occupation) {
+        GameObject gridSpaceObject;
+        GridSpace gridSpaceScript;
+        gridSpaceObject = Instantiate(hexPrefab, this.transform);
+        gridSpaceObject.name = string.Format("Hex Space {0},{1}", position.x, position.y);
+        gridSpaceObject.transform.localPosition = columnOffsetVector * position.x + rowOffsetVector * position.y;
+        gridSpaceScript = gridSpaceObject.GetComponent<GridSpace>();
+        gridSpaceScript.Position = position;
+        gridSpaceScript.Occupation = occupation;
+        gridSpaces.Add(position, gridSpaceScript);
+    }
+
+    void ShrinkGrid() {
+        Vector2Int adjacentPosition;
+        GridSpace adjacentSpace;
+        bool removeSpace;
+        List<Vector2Int> keysToRemove = new List<Vector2Int>();
+        foreach (KeyValuePair<Vector2Int, GridSpace> entry in gridSpaces) {
+            removeSpace = true;
+            if (entry.Value.Occupation == GridSpace.SpaceOccupation.Free) {
+                foreach (Vector2Int adjacency in GridManager.adjacencies) {
+                    adjacentPosition = entry.Key + adjacency;
+                    adjacentSpace = gridSpaces.ContainsKey(adjacentPosition) ? gridSpaces[adjacentPosition] : null;
+                    if (adjacentSpace != null && adjacentSpace.Occupation != GridSpace.SpaceOccupation.Free) {
+                        removeSpace = false;
+                        break;
+                    }
+                }
+                if (removeSpace) {
+                    keysToRemove.Add(entry.Key);
+                }
+            }
+        }
+        foreach (Vector2Int position in keysToRemove) {
+            Destroy(gridSpaces[position]);
+            gridSpaces.Remove(position);
+        }
+    }
+
+    public Material GetTokenMaterial(GridSpace.SpaceOccupation player) {
+        foreach (TokenColor tokenColor in tokenColors) {
+            if (tokenColor.player == player) {
+                return tokenColor.material;
+            }
+        }
+        return null;
+    }
+
+    public static GridManager GetGridManager() {
+        //Watch out for issues when changing scenes, variable needs to be reset somehow
+        if (GridManager.gridManager == null) {
+            GridManager.gridManager = GameObject.FindObjectOfType<GridManager>();
+        }
+        return GridManager.gridManager;
+    }
+}
+
+[Serializable]
+public struct TokenColor {
+    public GridSpace.SpaceOccupation player;
+    public Material material;
+}
---
+++ Hexes/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour
+{
+    private Color deselectedColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    private Color selectedColor = new Color(1.0f, 0.7f, 0.0f, 1.0f);
+    public enum SpaceOccupation {Free, Red, Blue}
+
+    public int X {get; set;}
+    public int Y {get; set;}
+    public Vector2Int Position
+    {
+        get {
+            return new Vector2Int(X, Y);
+        }
+        set {
+            X = value.x;
+            Y = value.y;
+        }
+    }
+    private SpaceOccupation occupation = SpaceOccupation.Free;
+    public SpaceOccupation Occupation
+    {
+        get {
+            return occupation;
+        }
+        set {
+            if (value == SpaceOccupation.Free && occupation != SpaceOccupation.Free) {
+                Free();
+            } else if (value != SpaceOccupation.Free && occupation == SpaceOccupation.Free) {
+                Occupy(value);
+            }
+        }
+    }
+    private GameObject token = null;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    void Free() {
+        occupation = SpaceOccupation.Free;
+        Destroy(token);
+        token = null;
+    }
+    void Occupy(SpaceOccupation newOccupation) {
+        occupation = newOccupation;
+        GridManager grid = GridManager.GetGridManager();
+        token = Instantiate(grid.tokenPrefab, transform);
+        SpriteRenderer spriteRenderer = token.GetComponent<SpriteRenderer>();
+        spriteRenderer.material = grid.GetTokenMaterial(newOccupation);
+    }
+    public bool IsAdjacent(GridSpace other) {
+        foreach (Vector2Int adjacency in GridManager.adjacencies) {
+            if (Position - other.Position == adjacency) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

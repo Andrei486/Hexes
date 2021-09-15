e2449a4663d619882208b0848aea7f721dcb5954
Andrei486 <apopescu486@gmail.com>
2021-09-15 15:42:53 -0400

    Implemented space selection, token addition & player switching for the same cursor

--- Hexes/Assets/Scripts/CursorController.cs
+++ Hexes/Assets/Scripts/CursorController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Extensions;
 
 public class CursorController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     public GridSpace.SpaceOccupation Player {get; set;}
     private Vector3 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,31 @@ public class CursorController : MonoBehaviour
     }
 
     public void OnSelect() {
-        Debug.Log("Selected");
+        Debug.Log("Pressed select");
+        GridSpace selectedSpace;
+        Collider2D[] colliders = new Collider2D[1];
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.useTriggers = true;
+        // LayerMask mask = LayerMask.NameToLayer("Grid");
+        // filter.SetLayerMask(mask);
+        //return only one collider: cursor hitbox should be small enough that the space between hexes
+        //guarantees that only one is selected at a time
+        int resultCount = Physics2D.OverlapCollider(GetComponent<Collider2D>(), filter, colliders);
+        Debug.Log(resultCount);
+        if (resultCount == 1) {
+            selectedSpace = colliders[0].gameObject.GetComponent<GridSpace>();
+            Debug.Log(selectedSpace.Position);
+            if (selectedSpace.Occupation == GridSpace.SpaceOccupation.Free) {
+                selectedSpace.Occupation = Player;
+                SwitchPlayer();
+                GridManager.GetGridManager().ExpandGrid();
+            }
+        }
+    }
+
+    public void SwitchPlayer() {
+        do {
+            Player = Player.Next<GridSpace.SpaceOccupation>();
+        } while (Player == GridSpace.SpaceOccupation.Free);
     }
 }

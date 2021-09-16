75b3d0b880a0e8deac0a207ba272aceaa2a93294
Andrei486 <apopescu486@gmail.com>
2021-09-16 13:09:50 -0400

    Improved camera controls, adding a manual movement option.

--- Hexes/Assets/Scripts/CameraController.cs
+++ Hexes/Assets/Scripts/CameraController.cs
@@ -5,29 +5,42 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
+    public static float MoveSpeed = 8.0f;
     public Camera camera;
-    private Vector3 target;
     private static float zoomFactor = 0.5f;
+    private Vector2 movement;
+    private Vector3? target;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        camera.transform.position = Vector3.MoveTowards(camera.transform.position, target, CursorController.MoveSpeed * 3f * Time.deltaTime);
+        if (target != null) {
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position, target.Value, MoveSpeed * 2f * Time.deltaTime);
+        }
     }
 
     public void OnSelect() {
         target = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
     }
 
+    public void OnMoveCamera(InputValue input) {
+        movement = input.Get<Vector2>() * MoveSpeed;
+        if (input.Get<Vector2>() != Vector2.zero) {
+            target = null;
+        }
+        camera.GetComponent<Rigidbody2D>().velocity = movement;
+    }
+
     public void OnZoomCamera(InputValue input) {
         float newSize = camera.orthographicSize - input.Get<float>() * zoomFactor;
         CursorController.MoveSpeed *= newSize/camera.orthographicSize;
+        MoveSpeed *= newSize/camera.orthographicSize;
         camera.orthographicSize = newSize;
     }
 }
